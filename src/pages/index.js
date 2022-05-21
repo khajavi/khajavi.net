a74cbe38fc53a7def8f37274eb6876b674288de1
Milad Khajavi <khajavi@gmail.com>
2022-05-21 11:17:46 +0430

    comment extra contents.

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,19 +19,19 @@ const BlogIndex = ({ data, location }) => {
     )
   })
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        {/*<Bio />*/}
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
+  // if (posts.length === 0) {
+  //   return (
+  //     <Layout location={location} title={siteTitle}>
+  //       <Seo title="All posts" />
+  //       {/*<Bio />*/}
+  //       <p>
+  //         No blog posts found. Add markdown posts to "content/blog" (or the
+  //         directory you specified for the "gatsby-source-filesystem" plugin in
+  //         gatsby-config.js).
+  //       </p>
+  //     </Layout>
+  //   )
+  // }
 
   return (
     <Layout location={location} title={siteTitle}>
